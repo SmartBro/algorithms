268888dcc24c3f46e4c2d1573bfb6eea5032e78a
SmartBro <smartbrostudio@gmail.com>
2021-02-16 13:33:47 +0200

    add more effective count inversions algorithm

--- divide_and_conqure/inversions.js
+++ divide_and_conqure/inversions.js
@@ -1,5 +1,3 @@
-const read = require('../utils/read');
-
 /**
  * Inefective brute force algorithm O(n^2)
  * @param {Number[]} input 
@@ -20,7 +18,52 @@ function countInversionsBruteForce(input) {
  * @param {Number[]} input
  */
 function countInversions(input) {
-    // TODO: implement
+    const [,inversions] = sortAndCountInversions(input);
+    return inversions;
+}
+
+/**
+ * 
+ * @param {Number[]} input
+ * @returns {Pair<Number[], Number>} [sorted, inversionsCount]
+ */
+function sortAndCountInversions(input) {
+    const n = input.length;
+    if (n < 2) return [input, 0];
+
+    const mid = Math.floor(n / 2);
+    const [left, leftInversions] = sortAndCountInversions(input.slice(0, mid));
+    const [right, rightInversions] = sortAndCountInversions(input.slice(mid));
+    const [sorted, splitInversions] = mergeAndCountSplitInversions(left, right);
+
+    return [sorted, leftInversions + rightInversions + splitInversions];
+}
+
+/**
+ * 
+ * @param {Number[]} left
+ * @param {Number[]} right
+ * @returns {Pair<Number[], Number>} sortedMerged splitInversions
+ */
+function mergeAndCountSplitInversions(left, right) {
+    const n = left.length + right.length;
+    const sorted = new Array(n);
+    let i = 0, j = 0, k = 0, splitInversions = 0;
+    for (; k < n && i < left.length && j < right.length; k++) {
+        if (left[i] < right[j]) sorted[k] = left[i++];
+        else {
+            sorted[k] = right[j++];
+            splitInversions += left.length - i;
+        }
+    }
+    while(i < left.length) {
+        sorted[k++] = left[i++];
+    }
+    while(j < right.length) {
+        sorted[k++] = right[j++];
+    }
+
+    return [sorted, splitInversions];
 }
 
 module.exports = {
